763621005793623c6893f7acfb2ace967b140d31
zewppie <abedwalid24@gmail.com>
2025-06-24 15:46:59 -0300

    Now uses I2C display and timer.

--- lights_out/lights_out.ino
+++ lights_out/lights_out.ino
@@ -1,9 +1,20 @@
 #include <stdbool.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define col 16
+#define lin 2
+#define ende 0x27
+
+LiquidCrystal_I2C lcd(ende,col,lin);
 
 String input = "";
 int inputNumber;
 bool gameState[9];
 
+int timer = 60;
+unsigned long previousMillis = 0; 
+const long interval = 1000;
 
 void setup() {
 
@@ -20,9 +31,16 @@ void setup() {
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 
 void loop() {
+  
+  decreaseTimer();
+
   if (Serial.available() > 0) {
     input = Serial.readStringUntil('\n');
     inputNumber = input.toInt();
@@ -81,9 +99,40 @@ void checkState(){
   }
   while(true) {
     light();
-    delay(1000);
-    for (int j = 0; j < 9; j++) {
-      gameState[j] = !gameState[j];
+    decreaseTimer();
+  }
+}
+
+void decreaseTimer(){
+  unsigned long currentMillis = millis(); // Pega o tempo atual
+
+  // Decrementa o timer a cada 1 segundo
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (timer > 0) {
+      timer--;
+    }
+
+    // Exibe o tempo restante no LCD
+    lcd.clear();
+    lcd.setCursor(0, 1); 
+    lcd.print("Timer: ");
+    lcd.print(timer);
+
+    if (timer == 0) {
+      lcd.setCursor(0, 1);
+      lcd.print("Game Over!");
+      // liga todas as luzes
+      for (int i = 0; i < 9; i++) {
+        gameState[i] = true;
+      }
+      light();
+      while (true) {
+
+        // aqui eu mando o sinal avisando que perdeu
+        
+      }
     }
   }
 }
